fe0439c2b5854e365f7b3157542274aeb5cbbe68
Ishit Godshalwar <ishit.godshalwar22@gmail.com>
2024-11-16 13:03:39 +0530

    added bubble_sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -15,6 +15,20 @@ void selection_Sort(int arr[],int n)
     }
 }
 
+void bubble_sort(int arr[], int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
 int main()
 {
     int arr[] = {5,2,1,9,3};
